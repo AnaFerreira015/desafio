665864524e90e553d855ef539ee4b53de6936cb4
AnaFerreira015 <dasilvaferreira.af@hotmail.com>
2018-07-13 14:38:14 -0300

    Atualiza o arquivo app.js

--- src/app.js
+++ src/app.js
@@ -1,39 +1,129 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+
+const bd = require('./firebase');
+
+const seriesRef = bd.child('serie');
+const filmesRef = bd.child('filme');
+const hqsRef = bd.child('hq');
+const livrosRef = bd.child('livro');
 
 const app = express();
-const router = express.Router();
-//falta conectar ao banco 
 
-//Carrega os models
-const Filme = require('./models/filme');
-const Serie = require('./models/serie');
-const Livro = require('./models/livro');
-const Hq = require('./models/hq');
-//Carrega as rotas
-const indexRoute = require('./routes/index-route');
-const filmeRoute = require('./routes/filme-route');
-const serieRoute = require('./routes/serie-route');
-const livroRoute = require('./routes/livro-controller');
-const hqRoute = require('./routes/hq-route');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
-
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.1"
-    });
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
+app.get('/series', (req, res) => {
+  seriesRef.once("value", (snap) => {
+    res.send(snap.val());
+  });
+});
 
+app.get('/filmes', (req, res) => {
+  filmesRef.once("value",(snap) => {
+    res.send(snap.val());
+  });
+});
 
-app.use('/', indexRoute);
-app.use('/filmes', filmeRoute);
-app.use('/series', serieRoute);
-app.use('/livros', livroRoute);
-app.use('/hqs', hqRoute);
+app.get('/hqs', (req, res) => {
+  hqsRef.once("value", (snap) => {
+    res.send(snap.val());
+  });
+});
 
+app.get('/livros', (req, res) => {
+  livrosRef.once("value", (snap) => {
+    res.send(snap.val());
+  });
+});
 
-module.exports = app;
+app.get('/series/name', (req, res) => {
+  var q = req.query.q;
+  seriesRef.orderByChild("nome").equalTo(q).once("value", (snap) => {
+    res.send(snap.val());
+  });
+});
+
+app.get('/series/year', (req, res) => {
+  var q = parseInt(req.query.q);
+  seriesRef.orderByChild("ano").equalTo(q).once("value", (snap) => {
+    res.send(snap.val());
+  });
+});
+
+app.get('/series/autorDir', (req, res) => {
+  var q = req.query.q;
+  seriesRef.orderByChild("autorDiretor").equalTo(q).once("value", (snap) => {
+    res.send(snap.val());
+  });
+});
+
+//filme
+app.get('/filmes/name', (req, res) => {
+  var q = req.query.q;
+  filmesRef.orderByChild("nome").equalTo(q).once("value", (snap) => {
+    res.send(snap.val());
+  });
+});
+
+app.get('/filmes/year', (req, res) => {
+  var q = parseInt(req.query.q);
+  filmesRef.orderByChild("ano").equalTo(q).once("value", (snap) => {
+    res.send(snap.val());
+  });
+});
+
+app.get('/filmes/autorDir', (req, res) => {
+  var q = req.query.q;
+  filmesRef.orderByChild("autorDiretor").equalTo(q).once("value", (snap) => {
+    res.send(snap.val());
+  });
+});
+
+//livro
+app.get('/livros/name', (req, res) => {
+  var q = req.query.q;
+  livrosRef.orderByChild("nome").equalTo(q).once("value", (snap) => {
+    res.send(snap.val());
+  });
+});
+
+app.get('/livros/year', (req, res) => {
+  var q = parseInt(req.query.q);
+  livrosRef.orderByChild("ano").equalTo(q).once("value", (snap) => {
+    res.send(snap.val());
+  });
+});
+
+app.get('/livros/autorDir', (req, res) => {
+  var q = req.query.q;
+  livrosRef.orderByChild("autorDiretor").equalTo(q).once("value", (snap) => {
+    res.send(snap.val());
+  });
+});
+
+//hq
+app.get('/hqs/name', (req, res) => {
+  var q = req.query.q;
+  hqsRef.orderByChild("nome").equalTo(q).once("value", (snap) => {
+    res.send(snap.val());
+  });
+});
+
+app.get('/hqs/year', (req, res) => {
+  var q = parseInt(req.query.q);
+  hqsRef.orderByChild("ano").equalTo(q).once("value", (snap) => {
+    res.send(snap.val());
+  });
+});
+
+app.get('/hqs/autorDir', (req, res) => {
+  var q = req.query.q;
+  hqsRef.orderByChild("autorDiretor").equalTo(q).once("value", (snap) => {
+    res.send(snap.val());
+  });
+});
+
+app.listen(8080, () => {
+  console.log('Example app listening on port 8080!');
+});
